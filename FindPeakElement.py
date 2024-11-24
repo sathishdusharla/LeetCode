43f752ccfa38261a6c4d84185d4f124d48078ea6
Sathish Dusharla <23eg105a16@anurag.edu.in>
2024-11-24 09:47:24 +0530

    Create FindPeakElement.py

---
+++ FindPeakElement.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, h = 0, len(nums) - 1
+        while l < h:
+            mid = l + (h - l) // 2
+            if nums[mid] > nums[mid + 1]: 
+                h = mid
+            else: 
+                l = mid + 1
+        return l 
